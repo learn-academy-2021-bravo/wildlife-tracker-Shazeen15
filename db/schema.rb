b841961797d47c8bc78a469e8b27cc5fe8a715d6
Shazeen15 <shazzy.sterlin@gmail.com>
2021-06-10 16:37:26 -0700

    generated sighting model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_221317) do
+ActiveRecord::Schema.define(version: 2021_06_10_232921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_06_10_221317) do
     t.string "common_name"
     t.string "latin_name"
     t.string "kingdom"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.datetime "date"
+    t.string "latitude"
+    t.string "longitude"
+    t.integer "animal_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
